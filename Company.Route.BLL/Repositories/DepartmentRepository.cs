4d3cd946ebf3df02d573e37c11ad4e7cf0c89162
Amr.Sameh <amrsameh903@gmail.com>
2024-09-12 02:09:19 +0300

    First Commit

---
+++ Company.Route.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,48 @@
+﻿using Company.Route.BLL.Interfaces;
+using Company.Route.DAL.Data.Contexts;
+using Company.Route.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Route.BLL.Repositories
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly AppDbContext _context;
+        public DepartmentRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public int Add(Department entity)
+        {
+            _context.Departments.Add(entity);
+            return _context.SaveChanges();
+        }
+
+        public int Delete(Department entity)
+        {
+            _context.Departments.Remove(entity);
+            return _context.SaveChanges();
+        }
+
+        public Department Get(int? id)
+        {
+            return _context.Departments.Find(id);
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            return _context.Departments.ToList();
+        }
+
+        public int Update(Department entity)
+        {
+            _context.Departments.Update(entity);
+            return _context.SaveChanges();
+        }
+    }
+}
---
+++ Company.Route.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,127 @@
+﻿using Company.Route.BLL.Interfaces;
+using Company.Route.BLL.Repositories;
+using Company.Route.DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.Route.PL.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public DepartmentController(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var departments = _departmentRepository.GetAll();
+            return View(departments);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Department model)
+        {
+            if(ModelState.IsValid)
+            {
+                var count = _departmentRepository.Add(model);
+
+                if (count > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(model);
+
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id is null)
+                return BadRequest();
+            var department = _departmentRepository.Get(id.Value);
+
+            if (department is null)
+                return NotFound();
+
+            return View(department);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id is null) return BadRequest();
+            var department = _departmentRepository.Get(id.Value);
+            if (department is null) return NotFound();
+            return View(department);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([FromRoute]int? id, Department model)
+        {
+            try
+            {
+                if (id != model.Id) return BadRequest();
+
+                if (ModelState.IsValid)
+                {
+                    var count = _departmentRepository.Update(model);
+                    if (count > 0)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id is null) return BadRequest();
+            var department = _departmentRepository.Get(id.Value);
+
+            if (department is null)return NotFound();
+
+            return View(department);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete([FromRoute] int? id, Department model)
+        {
+            try
+            {
+                if (id != model.Id) return BadRequest();
+                if (!ModelState.IsValid)
+                {
+                    var count = _departmentRepository.Delete(model);
+                    if (count > 0)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+            }
+
+            return View(model);
+        }
+
+    }
+}
+

d5817b91a12c8c69c123b2401e62bebce3562a45
Amr.Sameh <amrsameh903@gmail.com>
2024-09-19 00:42:33 +0300

    Revert AddEmployeeSection

--- Company.Route.BLL/obj/Debug/net8.0/Company.Route.BLL.AssemblyInfo.cs
+++ Company.Route.BLL/obj/Debug/net8.0/Company.Route.BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Company.Route.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c56e08c34548235d36ad2d2ac7dc120143305784")]
 [assembly: System.Reflection.AssemblyProductAttribute("Company.Route.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Company.Route.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
